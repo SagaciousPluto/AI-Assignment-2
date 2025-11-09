ab9bbadfc69cc321688d4705e0efb730e13a431d
Sagacious Pluto <moazzam.may@gmail.com>
2025-11-09 12:44:06 +0500

    All Data loaded

---
+++ data_loader.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+
+train_data = pd.read_parquet("E:\\Semester 3\\AI Labs\\task_a_training_set_1.parquet")
+val_data = pd.read_parquet("E:\\Semester 3\\AI Labs\\task_a_validation_set.parquet")
